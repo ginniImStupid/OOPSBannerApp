09178ab0bb6e60d301e5f5e7759813b82906de5e
ginniImStupid <jigmeepaotamang@gmail.com>
2026-03-12 23:07:33 +0530

    Coded UC2

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,11 +1,17 @@
 /* *
  * OOPS Banner App UC1
  * @author jigmeepaotamang
- * @version 1.0
+ * @version 2.0
  * */
 
 public class OOPSBannerApp {
   public static void main(String args[]) {
-    System.out.println("OOPS");
+    System.out.println("  *****     *****   ******    ******");
+    System.out.println(" **   **   **   **  **   **  **");
+    System.out.println("**     ** **     ** **   **  **");
+    System.out.println("**     ** **     ** ******    ****");
+    System.out.println("**     ** **     ** **           **");
+    System.out.println(" **   **   **   **  **           **");
+    System.out.println("  *****     *****   **      ******");
   }
 }
