749d6b1b9f86bde122358367c2790667b62814c1
ginniImStupid <jigmeepaotamang@gmail.com>
2026-03-13 16:00:06 +0530

    Coded UC6

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,22 +1,52 @@
 /* *
- * OOPS Banner App UC5
+ * OOPS Banner App UC6
  * @author jigmeepaotaman" "g
- * @version 5.0
+ * @version 6.0
  * */
 
 public class OOPSBannerApp {
-  public static void main(String args[]) {
-    String lines[] = {
-        String.join(" ", "  *****", "    *****", "  ******", "  ******"),
-        String.join(" ", " **   **", "  **    **", "**    **", "**   **"),
-        String.join(" ", "**     **", "**      **", "**   **", "**"),
-        String.join(" ", "**     **", "**      **", "******", "  ****"),
-        String.join(" ", "**     **", "**      **", "**", "        **"),
-        String.join(" ", " **   **", "  **    **", " **", "  * *   **"),
-        String.join(" ", "  *****", "    *****", "   **", "  ******"),
+  public static String[] getOPattern() {
+    return new String[] {
+        "   ******   ",
+        "  **    **  ",
+        " **      ** ",
+        " **      ** ",
+        " **      ** ",
+        "  **    **  ",
+        "   ******   ",
     };
-    for (String line : lines) {
-      System.out.println(line);
+  }
+
+  public static String[] getPPattern() {
+    return new String[] {
+        " ******  ",
+        " **   ** ",
+        " **   ** ",
+        " ******  ",
+        " **      ",
+        " **      ",
+        " **      ",
+    };
+  }
+
+  public static String[] getSPattern() {
+    return new String[] {
+        " ****** ",
+        " **  ** ",
+        " **     ",
+        "  ***** ",
+        "     ** ",
+        " **  ** ",
+        " ****** ",
+    };
+  }
+
+  public static void main(String args[]) {
+    String oPattern[] = getOPattern();
+    String pPattern[] = getPPattern();
+    String sPattern[] = getSPattern();
+    for (int i = 0; i < oPattern.length; i++) {
+      System.out.println(oPattern[i] + oPattern[i] + pPattern[i] + sPattern[i]);
     }
   }
 }
